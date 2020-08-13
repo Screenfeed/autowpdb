5eb5d377536211eddc99852715ca63d9085f43c7
Git I Hate You <git@screenfeed.fr>
2020-08-13 18:17:14 +0200

    Added basis for unit and integration tests Sadly, we're stuck with PHPUNIT 7.5.20 because of WP’s test suite 😢

---
+++ Tests/Integration/TestCase.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Screenfeed\AutoWPDB\Tests\Integration;
+
+use Brain\Monkey;
+use Screenfeed\AutoWPDB\Tests\TestCaseTrait;
+use WP_UnitTestCase;
+
+abstract class TestCase extends WP_UnitTestCase {
+	use TestCaseTrait;
+
+	/**
+	 * Prepares the test environment before each test.
+	 */
+	public function setUp() {
+		parent::setUp();
+		Monkey\setUp();
+	}
+
+	/**
+	 * Cleans up the test environment after each test.
+	 */
+	public function tearDown() {
+		Monkey\tearDown();
+		parent::tearDown();
+	}
+}
